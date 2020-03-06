dc5298242cf935a39ba4c7f7d695ca1dd552fe4e
yangemail8 <yangemail8@gmail.com>
2020-03-06 21:01:14 +0800

    #138 is done.

--- route/route.php
+++ route/route.php
@@ -19,6 +19,13 @@
 
 // ];
 
-// Route::post('api/:v.user/sendcode','api/v1.UserController/sendCode');
-// 发送验证码：post请求
-Route::post('user/sendcode','api/v1.UserController/sendCode');
+
+// 不需要验证Token
+Route::group('api/:version/', function(){
+    // 发送验证码
+    Route::post('user/sendcode', 'api/:version.UserController/sendCode');
+    // 手机登录
+    Route::post('user/phonelogin', 'api/:version.UserController/phoneLogin');
+});
+// 如果需要权限验证需要加上：
+// ->middleware(['ApiUserAuth']);
